bc3f81d09b125124eb2194e09450edcb4ddc3df3
fit <fitdafan@gmail.com>
2025-09-16 14:22:29 +0700

    bermain dengan view

--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Employees</title>
+    <title>Attendances</title>
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
 </head>
@@ -21,7 +21,6 @@
         </main>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmployeeController;
+use Illuminate\Http\Request;
 
-route::get('/', function() {
+Route::get('/', function () {
+    return view('sign');
+});
+
+Route::post('/sign', function (Request $request) {
+    $email = $request->input('email');
+    $password = $request->input('password');
+
+    if ($email === 'fitdafac@gmail.com' && $password === '15112005') {
+        return redirect('/employee')->with('success', 'Login berhasil!');
+    } else {
+        return back()->withErrors(['login' => 'Email atau password salah.']);
+    }
+});
+
+route::get('/employee', function() {
     return view('employee');
+});
+
+route::get('/department', function() {
+    return view('department');
 });
